35d02729060c25497dd842e1b3b36ad9ce4ccf9d
mohammad sadegh mes <mohammad sadegh mes>
2016-01-19 12:13:17 -0800

    add lesson two of php learn in repository

---
+++ lesson02PHP.php
@@ -0,0 +1,8 @@
+<?php
+echo "print numbers in screen with for loop ".'</br>'.'</br>'; 
+// for debug tools we will use die & var_dump
+// for loop struct
+for ($i=0; $i<=10 ; $i++){
+	echo $i.'</br>';	
+}
+?>
